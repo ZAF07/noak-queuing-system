208fd79d7603cfc25edf7cda03bb56d3a8c818f4
ZAF07 <zaffere07@gmail.com>
2023-08-05 13:37:59 +0800

    added plain algorithm for the bonus section

--- index.js
+++ index.js
@@ -1,85 +1,57 @@
-/* 
-This is the interactive version of the queuing system.
+// List of doctors ...
+const doctors = [
+  {
+    docName: "Dr Jack",
+    avgConsultTime: 3,
+    roomNumber: 1,
+  },
+  {
+    docName: "Dr Tim",
+    avgConsultTime: 5,
+    roomNumber: 2,
+  },
+];
 
-For simplicity, assume all the patients in the queue have no preference for the doctors they want to consult and all the doctors are available and not seeing any patient initially
+/**
+ * Calculates the estimated waiting time for a patient in a clinic queue.
+ *
+ * @param {number} qNum - The position of the patient in the queue.
+ * @param {Doctor[]} doctors - An array of Doctor objects representing available doctors.
+ * @returns {string} A formatted string with the patient's queue details, including wait time.
+ *
+ * @description
+ * The `calcWaitTime` function calculates the estimated waiting time for a patient
+ * based on their position in the queue and the average consultation time of the available doctors.
+ * It uses the Round Robin algorithm to assign patients to doctors in a cyclic manner.
+ *
+ * **Time Complexity:** O(1) - The function performs a fixed number of operations,
+ * regardless of the number of available doctors, resulting in constant time complexity.
+ */
+const calcWaitTime = (qNum, doctors) => {
+  // Get the number of doctors available in the clinic
+  const availDoctors = doctors.length;
 
-1. You can add/remove doctors from the clinic
-2. You can list all the doctors in the clinic
-3. You can get a patient's average waiting time to consultation
+  // Determine the patient's doctor
+  const assignedDoc = doctors[qNum % availDoctors];
 
-NOTE: To view the plain algorithm by without the interaction features (following rules as stated in the Bonus Section), checkout the 'bonus' branch: $ <git checkout bonus>
-*/
-import chalk from "chalk";
-import { question, questionInt } from "readline-sync";
-import Clinic from "./clinic.js";
-import Doctor from "./doctors.js";
+  // Get the assigned doctor's avg consult time
+  const avgConsultTime = assignedDoc.avgConsultTime;
 
-// TODO: Implement seperation of concerns here. This goes to utils but utils cannot know anything regarding the domain of the application
-// So we should initialise the Doctor class somewhere else
-const addDoctorToClinic = (clinic) => {
-  console.log(chalk.bgYellow("\n--- Add Doctor ---"));
-  const avgTime = parseInt(
-    question(
-      chalk.bgYellow.white("Enter average consultation time (in minutes): \t")
-    )
-  );
-  const doctorName = question(chalk.bgYellow("Enter doctor name: \t"));
-
-  const newDoctor = new Doctor(avgTime, doctorName);
-  clinic.addDoctor(newDoctor);
-
-  console.log(chalk.bgGreenBright("Doctor added successfully!\n"));
-};
-
-const removeDoctor = (clinic) => {
-  console.log(chalk.bgYellowBright("\n--- Remove Doctor ---"));
-  const docName = question(chalk.bgRedBright("Enter Doctor name: \t"));
-  clinic.removeDoctor(docName);
-};
-
-const getWaitTime = (clinic) => {
-  console.log(chalk.bgYellowBright("\n--- Get average wait time ---"));
-  const qNum = parseInt(
-    question(chalk.bgYellowBright("Enter your Q number: \t"))
-  );
-  clinic.getWaitTime(qNum);
-};
-const main = () => {
-  const clinic = new Clinic();
-
-  while (true) {
-    console.log(chalk.bgGrey("\n--- Clinic Management Menu ---"));
-    console.log("1. Add Doctor");
-    console.log("2. List Doctors");
-    console.log("3. Remove doctor");
-    console.log("4. Get average waiting time");
-    console.log("0. Exit");
-
-    const choice = questionInt("Enter your choice: ");
-
-    switch (choice) {
-      case 1:
-        addDoctorToClinic(clinic);
-        break;
-      case 2:
-        clinic.listDoctors();
-        break;
-      case 3:
-        removeDoctor(clinic);
-        break;
-      case 4:
-        getWaitTime(clinic);
-        break;
-      case 0:
-        console.log("Exiting...");
-        process.exit(0);
-      default:
-        console.log(
-          chalk.redBright(
-            "Invalid choice. Read the options and enter a valid one."
-          )
-        );
-    }
+  // Calculate the wait time
+  let estWaitTime = avgConsultTime * Math.floor((qNum - 1) / availDoctors);
+  if (qNum === 1) {
+    estWaitTime = 0;
   }
+
+  //  Calculate the total number of patients ahead of current patient..
+  const patientsAhead = Math.floor((qNum - 1) / availDoctors);
+  const docName = assignedDoc.docName;
+  const roomNumber = assignedDoc.roomNumber;
+  console.log(
+    `\nAt Q number ${5}, with ${availDoctors} available doctors with varying consult time, here are your Q details: \n`
+  );
+  return `\nDoctor assigned: ${docName}, Room number: ${roomNumber}, Average consult time: ${avgConsultTime}, Patients ahead of you: ${patientsAhead}, Estimated wait time: ${estWaitTime}\n`;
 };
-main();
+
+const result = calcWaitTime(5, doctors);
+console.log("response: ", result);
